5e952796a12ec097623931abdc11abe102f2d9ae
Alex Hong <78110944+Alexhong1269@users.noreply.github.com>
2023-07-05 21:14:55 -0400

    Add files via upload

---
+++ winner.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>WINNER</title>
+    <link href="endGame.css"  rel="stylesheet">
+</head>
+<body>
+    <div>
+        <p class="bannerText"><strong>WHO WANTS TO BE A MILLIONAIRE<strong></p>
+        <br/>
+        <img src="./millionaire.avif" alt="logo" class="banner"/>
+        <br/>
+        <p class="ending"><strong>You Win!!!<strong></p>
+
+    <!--This is the hyperlink back to the main page-->
+    <p class="homeLink"><a href="index.php">Back to Main Page</a></p>
+
+    </div>
+</body>
+</html>

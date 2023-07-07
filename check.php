244eee0565649c512fae66fec5965748b9f47be0
Aaron Grizzle <119636980+adgrizzle@users.noreply.github.com>
2023-07-07 12:23:21 -0400

    Update check.php

--- check.php
+++ check.php
@@ -2,17 +2,23 @@
 	require_once "common.php";
 	session_start();
 	
-	// Check whether the provided answer is correct.
+	// If the provided answer is correct,
+	// increment the current question number.
 	$answer = $_POST["answer"];
 	$is_correct = check_answer($answer);
 	if ($is_correct) $_SESSION["q_num"] += 1;
+	
+	// Otherwise, go the lose page.
 	else {
 		header("Location: lose.php");
 		exit();
 	}
 	
-	// Check whether the player has correctly answered all questions.
+	// If the player has not correctly answered 15 questions,
+	// go to the next question.
 	if ($_SESSION["q_num"] <= 15) header("Location: question.php");
+	
+	// Otherwise, go to the winner page.
 	else {
 		header("Location: winner.php");
 		exit();
